3779b7d2ca13cf28168a41b5b2038927db18d652
satyam <satyampayal10@gmail.com>
2023-05-09 20:08:45 +0530

    express intro

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express=require('express');
 const app = express();
 
-app.get('',(req,res)=>{
-    res.send("this,is home page");
+app.get("",(req,res)=>{
+    console.log("send data by Browser",req.query);
+    res.send("this,is home page,"+req.query.name);
+
 });
-app.get('/Aboutus ',(req,res)=>{
+
+app.get("/about ",(req,res)=>{
+    res.send("this,isAbout us page");
+});
+
+app.get("/help ",(req,res)=>{
     res.send("this,isAbout us page");
 });
 
