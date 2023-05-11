f28453b71819ad4c06fd21c7bc438863f9e69346
satyam <satyampayal10@gmail.com>
2023-05-11 12:30:04 +0530

    Middleware

--- data.js
+++
@@ -1,9 +0,0 @@
-const data=[
-    {name:"satyam", email:"abc@gmail.com"},
-    {name:"payal",email:"xyz@gmail.com"},
-    {name:"payal",email:"xyz@gmail.com"},
-    {name:"payal",email:"xyz@gmail.com"},
-    {name:"payal",email:"xyz@gmail.com"}
-]
-
-module.exports=data;
